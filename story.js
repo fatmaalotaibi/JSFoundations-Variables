01a7999bf0530e432c3806b1d9d338a76eb4e97c
FN Otaibi <nsayemlil@hotmail.com>
2020-06-15 20:25:41 +0300

    created JSF.js

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+const name = "Fatma";
 // Define the age here on line 4
-
+const age = 33;
 // Define the hobby here on line 6
-
+const hoppy = "reading";
 // Print the story here on line 8
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hoppy} practice!`
+);
